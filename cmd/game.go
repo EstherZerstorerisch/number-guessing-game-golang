7e0d42f058f28551a8d0bb717a23bf257e81a06f
EstherZerstorerisch <hieuvn2512@gmail.com>
2025-09-04 20:58:16 +1000

    Adding more functions to the game

--- cmd/game.go
+++ cmd/game.go
@@ -2,37 +2,67 @@ package cmd
 
 import (
 	"fmt"
+	"log"
 	"math/rand/v2"
 )
 
-func RandomNum() int {
-	return rand.IntN(101)
+func RandomNum() uint8 {
+	return uint8(rand.IntN(101))
 }
 
-func DiffLevel() (difficulty string) {
-	fmt.Println(`
-	Please select the difficulty level:
-	1. Easy (10 chances)
-	2. Medium (5 chances)
-	3. Hard (3 chances)
-	`)
-	// Prompt use to input number
-	var choice int
-	fmt.Scanln(&choice)
-
+func DiffLevel() (chances uint8) {
+	var choice uint8
+	var difficulty string
+	
+	// Prompt user to input number
+	fmt.Print("Please select the difficulty level:\n1. Easy (10 chances)\n2. Medium (5 chances)\n3. Hard (3 chances)\n\n")
+	fmt.Print("Enter your choice: ")
+	_, err := fmt.Scanln(&choice)
+	if err != nil {
+		log.Fatalln("Invalid input!")
+	}
+	fmt.Println()
 	// Use switch statement
 	switch choice {
 	case 1:
 		difficulty = "Easy"
+		chances = 10
 	case 2:
 		difficulty = "Medium"
+		chances = 5
 	case 3:
 		difficulty = "Hard"
+		chances = 3
 	default:
-		fmt.Println("Wrong! Please chose again!")
+		log.Fatal("Wrong! Please chose again!\n\n")
 		DiffLevel()
 	}
-	fmt.Printf("Great! You have selected the %s difficulty level", difficulty)
+	if difficulty != "" {
+		fmt.Printf("Great! You have selected the %s difficulty level\n", difficulty)
+	}
 	return 
 }
 
+func GuessNumber() (guess uint8) {
+	fmt.Print("Enter your guess: ")
+	_, err := fmt.Scanln(&guess)
+	if err != nil {
+		log.Fatal("Invalid input")
+	}
+	return
+}
+
+func CompareAnswer(userInput, answer uint8, chances *uint8) bool {
+	if userInput == answer {
+		fmt.Printf("Congratulations! You guessed the correct number in %d attempts.\n", *chances)
+		return true
+	} else if userInput > answer {
+		fmt.Printf("Incorrect! The number is less than %d.\n", userInput)
+		*chances --
+		return false
+	} else {
+		fmt.Printf("Incorrect! The number is greater than %d.\n", userInput)
+		*chances --
+		return false
+	}
+}
--- main.go
+++ main.go
@@ -2,11 +2,50 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"strings"
 
 	"github.com/EstherZerstorerisch/number-guessing-game-golang/cmd"
 )
 
 func main() {
-	chosenNum := cmd.RandomNum()
-	fmt.Printf("My number: %d", chosenNum)
+	// Game introduction
+	fmt.Println("Welcome to the Number Guessing Game!")
+	fmt.Print("I'm thinking of a number between 1 and 100.\n\n")
+	
+	restart := true
+	for restart {
+		// Create a random number
+		chosenNum := cmd.RandomNum()
+		fmt.Println(chosenNum)
+		// Let user choose difficulty level then return remaining chances
+		guessingChances := cmd.DiffLevel()
+		fmt.Print("Let's start the game!\n\n")
+
+		// User guessing number
+		userAnswer := cmd.GuessNumber()
+		
+		// Compare to the result
+		for !cmd.CompareAnswer(userAnswer, chosenNum, &guessingChances) {
+			userAnswer = cmd.GuessNumber()
+			if guessingChances == 0 {
+				fmt.Print("You lose!\n\n")
+			}
+		}
+		// Ask user if want to replay
+		var play string
+		fmt.Print("Do you want to start new game? (y/n) ")
+		_, err := fmt.Scanln(&play)
+		fmt.Println()
+		if err != nil {
+			log.Fatal("Invalid input.")
+		}
+
+		switch strings.ToLower(play) {
+		case "yes": restart = true
+		case "y": restart = true
+		case "no": restart = false
+		case "n": restart = false
+		}
+	}
 }
